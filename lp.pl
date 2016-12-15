9397b241d52d399e5cb759f1975633bb6c4a559c
Willian Lauber <lcwillian_hd@hotmail.com>
2016-12-15 08:01:33 -0200

    Corrected the comments
    The code was changed, but the comments don't.
    Now it's updated

--- lp.pl
+++ lp.pl
@@ -1,8 +1,8 @@
 % Squadron(-Solution)
 % @param Solution is a list of houses that satisfy all constraints.
 % @author Willian Lauber
-%falta O avião que solta fumaça verde está imediatamente à direita do avião que solta fumaça branca.
 % @see https://rachacuca.com.br/logica/problemas/esquadrilha-de-avioes/
+
 %pesquisas
 % @param Bebidae é o parametro de busca, digita-se Piloto(com p maiusculo) e o nome de uma bebida.
 % @retorn é retornado quem bebe tal bebida, se nimguem beber ela retorna falso.Obs funciona para encontrar o que bebe um piloto também.
@@ -18,6 +18,7 @@ member(aircraft(Piloto,_,_, _,Esporte), SA).
 % ) é o parametro de busca.
 % @retorn é retornado o piloto que possui essa caracteristica, retorna falso
 % se ninguem possuir.
+
 nomepiloto(Piloto, X):-
 squadron(SA),
 member(aircraft(Piloto,Cor,Problema, Bebida, Esporte), SA), (
@@ -35,6 +36,7 @@ member(aircraft(Piloto,_,_, _,natacao), SA).
 problemanabussola(Piloto):-
 squadron(SA),
 member(aircraft(Piloto,_,bussola, _, _), SA).
+
 % rules
 % 1 O avião do Cel. Milton solta fumaça vermelha.
 % 2 O rádio transmissor do Ten. Walter está com problemas.
@@ -51,6 +53,7 @@ member(aircraft(Piloto,_,bussola, _, _), SA).
 % 13 O Cap. Nascimento bebe somente água.
 % 14 O Cap. Farfarelli voa ao lado do avião que solta fumaça azul.
 % 15 Na formação, há um avião entre o que tem problema hidráulico e o com pane no altímetro.
+
 % Squadron of aircraft
 % regra para dizer que a esta ao lado de b, ou seja é seu vizinho
 % caso "a" for encontrado antes e/ou depois de "b", "ab" é onde os dois se encontram
@@ -64,6 +67,7 @@ onebetween(A, B, AB) :- neighbor(X, A, AB), neighbor(X, B, AB), A \=B.
 % aircraft(piloto, cor, anomalia, bebida, esporte).
 % a esquadrilha-de-avioes é composta de 5 aviões por isso length(SA, 5)
 squadron(SA) :-
+
 %os numeros se referem às dicas expostas anteriormente.
 length(SA, 5),
 member(aircraft(milton, vermelha, _, _, _), SA),%1
@@ -83,36 +87,42 @@ neighbor(aircraft(farfarelli,_,_,_,_), aircraft(_,azul,_,_,_), SA),%14
 onebetween(aircraft(_,_,hidraulico,_,_), aircraft(_,_, altimetro,_,_), SA), %15
 % os valores possiveis estão a seguir listados.
 % é definido no prolog que algum membro da lista possui tal atributo
+
 %pilotos
 member(aircraft(farfarelli, _, _, _, _), SA),
 member(aircraft(milton, _, _, _, _), SA),
 member(aircraft(nascimento, _, _, _, _), SA),
 member(aircraft(rui, _, _, _, _), SA),
 member(aircraft(walter, _, _, _, _), SA),
+
 %cores
 member(aircraft(_, azul, _, _, _), SA),
 member(aircraft(_, branca, _, _, _), SA),
 member(aircraft(_, verde, _, _, _), SA),
 member(aircraft(_, vermelha, _, _, _), SA),
 member(aircraft(_, preta, _, _, _), SA),
+
 %defeitos
 member(aircraft(_,_, altimetro, _, _), SA),
 member(aircraft(_,_, bussola, _, _), SA),
 member(aircraft(_,_, hidraulico, _, _), SA),
 member(aircraft(_,_, radio, _, _), SA),
 member(aircraft(_,_, temperatura, _, _), SA),
+
 %bebidas
 member(aircraft(_,_, _, agua, _), SA),
 member(aircraft(_,_, _, cafe, _), SA),
 member(aircraft(_,_, _, cha, _), SA),
 member(aircraft(_,_, _, cerveja, _), SA),
 member(aircraft(_,_, _, leite, _), SA),
+
 %esportes
 member(aircraft(_,_, _, _, equitacao), SA),
 member(aircraft(_,_, _, _, futebol), SA),
 member(aircraft(_,_, _, _, natacao), SA),
 member(aircraft(_,_, _, _, pesca), SA),
 member(aircraft(_,_, _, _, tenis), SA).
+
 % consultas
 %
 % ?- squadron(X).
